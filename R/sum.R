0e866ab8b9c4c7c41d49192fb9d1da8e064512c6
dlsimpao <dlsimpao@yahoo.com>
2020-02-26 09:52:43 -0800

    init

---
+++ R/sum.R
@@ -0,0 +1,13 @@
+#' adds two numbers
+#'
+#' @param a numeric
+#' @param b numeric
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples sum(1,2)
+#'
+sum = function(a,b){
+  as.numeric(a) + as.numeric(b)
+}
